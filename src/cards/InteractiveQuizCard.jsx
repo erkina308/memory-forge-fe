ebd6a8ef4ffb6d55d2a409d6f3399ac6e8800ce9
erkina308 <erkinayna@hotmail.co.uk>
2024-03-24 15:56:20 +0000

    functionality and styling for actual interactive quiz

--- src/cards/InteractiveQuizCard.jsx
+++ src/cards/InteractiveQuizCard.jsx
@@ -1,44 +1,111 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-export default function InteractiveQuizCard({ quiz, quizAnswer }) {
-  const [correctChoiceClicked, setCorrectChoiceClicked] = useState(false);
-  const [selectedChoice, setSelectedChoice] = useState(0);
+export default function InteractiveQuizCard({ quizzes }) {
+  let [index, setIndex] = useState(0);
+  const [question, setQuestion] = useState(quizzes[index]);
+  const [lock, setLock] = useState(false);
+  const [result, setResult] = useState(false);
 
-  function handleChoice(choice) {
-    setSelectedChoice(choice);
-    if (selectedChoice === quizAnswer) {
-      setCorrectChoiceClicked(true);
-    } else {
-      setCorrectChoiceClicked(false);
+  const OptionOne = useRef(null);
+  const OptionTwo = useRef(null);
+  const OptionThree = useRef(null);
+  const OptionFour = useRef(null);
+
+  const optionArr = [OptionOne, OptionTwo, OptionThree, OptionFour];
+
+  const checkAnswer = (e, ans) => {
+    if (lock === false) {
+      if (question.correct_answer === ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+      } else {
+        e.target.classList.add("incorrect");
+        setLock(true);
+        optionArr[
+          question.choices.indexOf(question.correct_answer)
+        ].current.classList.add("correct");
+      }
     }
-  }
+  };
+
+  const nextQ = () => {
+    if (lock === true) {
+      if (index === quizzes.length - 1) {
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(quizzes[index]);
+      setLock(false);
+      optionArr.map((option) => {
+        option.current.classList.remove("incorrect");
+        option.current.classList.remove("correct");
+      });
+    }
+  };
+
+  const resetQ = () => {
+    setIndex(0);
+    setQuestion(quizzes[0]);
+    setLock(false);
+    setResult(false);
+  };
+
   return (
-    <div key={quiz.quiz_id} className="quiz-card-container">
+    <div className="quiz-card-container">
       <div className="quiz-card">
-        <div className="interactive-quiz-container">
-          <h1 className="quiz-card-question interactive-question">
-            {quiz.question}
-          </h1>
-          <div className="interactive-quiz-card-grid">
-            {quiz.choices.map((choice, index) => {
-              return (
-                <div
-                  className={`quiz-card-list interactive-card-list ${
-                    correctChoiceClicked
-                      ? "selected-correct-choice"
-                      : "selected-incorrect-choice"
-                  }`}
-                  key={index}
-                  onClick={() => handleChoice(choice)}
-                >
-                  <ul>
-                    <li>{choice}</li>
-                  </ul>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        {result ? (
+          <></>
+        ) : (
+          <>
+            <h1 className="quiz-card-question interactive-question">
+              {index + 1}. {question.question}
+            </h1>
+            <ul className="interactive-quiz-list-container">
+              <li
+                ref={OptionOne}
+                onClick={(e) => {
+                  return checkAnswer(e, question.choices[0]);
+                }}
+              >
+                {question.choices[0]}
+              </li>
+              <li
+                ref={OptionTwo}
+                onClick={(e) => {
+                  return checkAnswer(e, question.choices[1]);
+                }}
+              >
+                {question.choices[1]}
+              </li>
+              <li
+                ref={OptionThree}
+                onClick={(e) => {
+                  return checkAnswer(e, question.choices[2]);
+                }}
+              >
+                {question.choices[2]}
+              </li>
+              <li
+                ref={OptionFour}
+                onClick={(e) => {
+                  return checkAnswer(e, question.choices[3]);
+                }}
+              >
+                {question.choices[3]}
+              </li>
+            </ul>
+            <button onClick={nextQ}>Next Question</button>
+          </>
+        )}
+        {result ? (
+          <>
+            <h2>Well done for completing the quiz</h2>
+            <button onClick={resetQ}>Reset</button>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
