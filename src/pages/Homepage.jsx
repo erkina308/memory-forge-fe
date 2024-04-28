ef10dfe9ff38b15493259cd7042cbfe029602a08
erkina308 <erkinayna@hotmail.co.uk>
2024-04-28 15:57:24 +0100

    homepage styled

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
   return (
     <section>
-      <h1>Welcome to Memory Forge</h1>
-      <div>
-        <button>
-          <Link to={"/register"}>Create Account</Link>
-        </button>
-        <button>
-          <Link to={"/login"}>Sign In</Link>
-        </button>
+      <div className="homepage_container">
+        <h1 className="homepage_title">Welcome to Memory Forge</h1>
+        <div className="homepage_button_container">
+          <button className="homepage_btn">
+            <Link to={"/register"}>Create Account</Link>
+          </button>
+          <button className="homepage_btn">
+            <Link to={"/login"}>Sign In</Link>
+          </button>
+        </div>
       </div>
     </section>
   );
